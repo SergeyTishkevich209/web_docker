ac0391f69972f623c3a97a6a45ee8f8b191e89ad
SergeyTishkevich <SergeyTishkevich@coherentsolutions.com>
2021-03-22 17:46:03 +0300

    my first app with docker

--- client/client.py
+++ client/client.py
@@ -1,22 +1,30 @@
 from flask import Flask, json
+from datetime import datetime
 import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
-    data = requests.get('http://docker-app_serv1_1:8080/info')
-    our_dict = data.json()
-    our_dict['machine'] = 'client'
+    try:
+        data = requests.get('http://web-docker_superserver_1:8080').json()
+    except requests.exceptions.ConnectionError as e:
+        data = {}
+    our_dict = {
+        'string': 'this is json',
+        'time': str(datetime.utcnow()),
+        'random id': data.get('number'),
+        'machine': 'server'
+    }
     response = app.response_class(
         response=json.dumps(our_dict, sort_keys=False, indent=4),
         status=200,
         mimetype='application/json'
     )
     return response
-
 
 if __name__ == '__main__':
     app.run(debug=True)
